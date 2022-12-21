04e8bb24e99113527f675de71459c3d53fa73de7
Actat <akio.toyosh@gmail.com>
2022-12-21 11:36:07 +0900

    type narrowing

--- packages_rs/nextclade-web/src/components/Main/RunOnLoad.tsx
+++ packages_rs/nextclade-web/src/components/Main/RunOnLoad.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 export function RunOnLoad() {
   const url = new URL(window.location.href)
   const params = url.searchParams
-  const fastaUrl = params.get('fasta')
+  const fastaParam = params.get('fasta')
+  if (typeof fastaParam !== 'string') {
+    return <div />
+  }
+  const fastaUrl: string = fastaParam
   console.log(fastaUrl) // eslint-disable-line no-console
+
   return <div />
 }
