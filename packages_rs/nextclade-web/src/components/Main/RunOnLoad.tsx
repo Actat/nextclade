fa8bc7722bc0895317c874b96af1b79da785d6dc
Actat <akio.toyosh@gmail.com>
2022-12-21 15:59:43 +0900

    add query and run

--- packages_rs/nextclade-web/src/components/Main/RunOnLoad.tsx
+++ packages_rs/nextclade-web/src/components/Main/RunOnLoad.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { AlgorithmInputUrl } from 'src/io/AlgorithmInput'
+import { useQuerySeqInputs } from 'src/state/inputs.state'
+import { useRunAnalysis } from 'src/hooks/useRunAnalysis'
 
 export function RunOnLoad() {
+  const { qryInputs, addQryInputs } = useQuerySeqInputs()
+  const run = useRunAnalysis()
+
   const url = new URL(window.location.href)
   const params = url.searchParams
   const fastaParam = params.get('fasta')
@@ -9,6 +15,14 @@ export function RunOnLoad() {
   }
   const fastaUrl: string = fastaParam
   console.log(fastaUrl) // eslint-disable-line no-console
+  const ai = new AlgorithmInputUrl(fastaUrl)
+  console.log(ai) // eslint-disable-line no-console
+
+  console.log(qryInputs) // eslint-disable-line no-console
+  if (!qryInputs.some((element) => element.type === 'Url' && element.name === fastaUrl)) {
+    addQryInputs([ai])
+    run()
+  }
 
   return <div />
 }
