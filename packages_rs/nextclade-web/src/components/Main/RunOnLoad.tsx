da810e93780de40af5a1c6e0d13523d4b45c7f36
Actat <akio.toyosh@gmail.com>
2022-12-21 10:50:09 +0900

    get url parameter

--- packages_rs/nextclade-web/src/components/Main/RunOnLoad.tsx
+++ packages_rs/nextclade-web/src/components/Main/RunOnLoad.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export function RunOnLoad() {
-  console.log('Hello World') // eslint-disable-line no-console
+  const url = new URL(window.location.href)
+  const params = url.searchParams
+  const fastaUrl = params.get('fasta')
+  console.log(fastaUrl) // eslint-disable-line no-console
   return <div />
 }
